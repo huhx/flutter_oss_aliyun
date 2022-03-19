20797962652d71f4bbc31c0064912c75015241dd
hongxiang <huxhu@thoughtworks.com>
2022-03-19 12:45:23 +0800

    [huhx] -feat: simplify the process when we put object

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter_oss_aliyun/src/request.dart';
 import 'package:mime_type/mime_type.dart';
 
@@ -7,49 +8,85 @@ import 'auth.dart';
 import 'encrypt.dart';
 
 class Client {
+  static Client? _instance;
+
+  factory Client() => _instance!;
+
   final String stsRequestUrl;
   final String endpoint;
+  final String bucketName;
   final Function(String) tokenGetter;
 
-  Client(this.stsRequestUrl, this.endpoint, this.tokenGetter);
+  Client._(this.stsRequestUrl, this.endpoint, this.bucketName, this.tokenGetter);
+
+  static void init({
+    required String stsUrl,
+    required String ossEndpoint,
+    required String bucketName,
+  }) {
+    _instance = Client._(stsUrl, ossEndpoint, bucketName, (url) async {
+      final response = await Dio().get<String>(url);
+      return response.data!;
+    });
+  }
 
   Auth? _auth;
   String? _expire;
 
-  Future<Client> getAuth() async {
+  Future<Auth> getAuth() async {
     if (_isNotAuthenticated()) {
       final resp = await tokenGetter(stsRequestUrl);
       final respMap = jsonDecode(resp);
       _auth = Auth(respMap['AccessKeyId'], respMap['AccessKeySecret'], respMap['SecurityToken']);
       _expire = respMap['Expiration'];
     }
-    return this;
+    return _auth!;
   }
 
-  HttpRequest getObject(String bucketName, String fileKey) {
+  Future<Response<dynamic>> getObject(String fileKey, {String? bucketName}) async {
+    bucketName ??= this.bucketName;
+    final auth = await getAuth();
+
     final url = "https://$bucketName.$endpoint/$fileKey";
-    var req = HttpRequest(url, 'GET', {}, {});
-    _auth!.sign(req, bucketName, fileKey);
-    return req;
+    var request = HttpRequest(url, 'GET', {}, {});
+    auth.sign(request, bucketName, fileKey);
+
+    return Dio().request(
+      request.url,
+      data: MultipartFile.fromBytes(request.fileData).finalize(),
+      options: Options(headers: request.headers, method: request.method),
+    );
   }
 
-  HttpRequest putObject(List<int> fileData, String bucketName, String fileKey) {
+  Future<Response<dynamic>> putObject(List<int> fileData, String fileKey, {String? bucketName}) async {
+    bucketName ??= this.bucketName;
+    final auth = await getAuth();
+
     final headers = {
       'content-md5': EncryptUtil.md5File(fileData),
       'content-type': mime(fileKey),
     };
     final url = "https://$bucketName.$endpoint/$fileKey";
-    HttpRequest req = HttpRequest(url, 'PUT', {}, headers);
-    _auth!.sign(req, bucketName, fileKey);
-    req.fileData = fileData;
-    return req;
+    HttpRequest request = HttpRequest(url, 'PUT', {}, headers);
+    auth.sign(request, bucketName, fileKey);
+    return Dio().request(
+      request.url,
+      data: MultipartFile.fromBytes(fileData).finalize(),
+      options: Options(headers: request.headers, method: request.method),
+    );
   }
 
-  HttpRequest deleteObject(String bucketName, String fileKey) {
+  Future<Response<dynamic>> deleteObject(String fileKey, {String? bucketName}) async {
+    bucketName ??= this.bucketName;
+    final auth = await getAuth();
+
     final url = "https://$bucketName.$endpoint/$fileKey";
-    final req = HttpRequest(url, 'DELETE', {}, {});
-    _auth!.sign(req, bucketName, fileKey);
-    return req;
+    final request = HttpRequest(url, 'DELETE', {}, {});
+    auth.sign(request, bucketName, fileKey);
+    return Dio().request(
+      request.url,
+      options: Options(headers: request.headers, method: request.method),
+    );
   }
 
   bool _isNotAuthenticated() {
