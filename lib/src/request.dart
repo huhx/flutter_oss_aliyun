77ce423d7397a39f60d475aa397760067eb24f91
hongxiang <gohuhx@163.com>
2023-01-06 11:46:53 +0800

    [huhx] -feat: remove option in HttpRequest

--- lib/src/request.dart
+++ lib/src/request.dart
@@ -1,23 +1,19 @@
-import 'request_option.dart';
-
 class HttpRequest {
   final String _url;
   final String method;
   final Map<String, dynamic> param;
   final Map<String, dynamic> headers;
-  final PutRequestOption? option;
 
   HttpRequest(
     this._url,
     this.method,
     this.param,
-    this.headers, {
-    this.option,
-  });
+    this.headers,
+  );
 
   String get url {
-    final urlString =
+    final queryString =
         param.entries.map((entry) => "${entry.key}=${entry.value}").join("&");
-    return urlString.isEmpty ? _url : "$_url?$urlString";
+    return queryString.isEmpty ? _url : "$_url?$queryString";
   }
 }
